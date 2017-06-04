3d76509489147ff51dff526e2159a6951122f21e
joacols <joaco.l.s@hotmail.com>
2017-06-03 21:26:15 -0300

    empiezo con la clase abstracta equipo, mi idea de porque esta en un comentario en la clase.

---
+++ DragonAlgoBall/src/dab/equipos/Equipo.java
@@ -0,0 +1,26 @@
+package dab.equipos;
+import dab.personajes.*;
+
+import java.util.HashMap;
+import java.util.Map;
+public abstract class Equipo {
+	/*contiene a los personajes, y a su vez,  podria ser que los personajes contengan al equipo.
+	 * asi por ejemplo para la habilidad de picollo y gohan podemos acceder a los personajes del equipo de
+	 * forma rapida
+	 */
+	
+	Map<String, Personaje> personajes = new HashMap<String, Personaje>();
+	int esferasDelDragon, personajesVivos;
+	
+	public boolean esGanador(){
+		if (esferasDelDragon >= 7)
+			return true;
+		return false;	
+	}
+	
+	public boolean esPerdedor(){
+		if(personajesVivos == 0)
+			return true;
+		return false;	
+	}		
+}
