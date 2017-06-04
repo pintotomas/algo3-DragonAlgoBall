211d287638b141235424f5ab101356c5b585a575
cuenta2 <tomasp834@gmail.com>
2017-06-04 19:33:49 -0300

    Añado mas Pruebas tablero

--- DragonAlgoBall/src/dab/tests/movimiento/PruebasTablero.java
+++ DragonAlgoBall/src/dab/tests/movimiento/PruebasTablero.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import dab.juego.Tablero;
+import dab.personajes.gohan.Gohan;
 import dab.personajes.goku.Goku;
-
+import dab.dragonBallExceptions.MovimientoInvalido;
 public class PruebasTablero{
 
 	@Test
@@ -19,7 +20,7 @@ public class PruebasTablero{
 		
 	}
 	@Test
-	public void testMoverseDesplazaPersonaje(){
+	public void testMoverPersonajeLoDesplaza(){
 		Tablero tableroDab = new Tablero();
 		Goku goku = new Goku();
 		tableroDab.colocarPersonaje(5, 6, goku);
@@ -28,7 +29,7 @@ public class PruebasTablero{
 		assertEquals(tableroDab.columnaPersonaje(goku), 5);
 	}
 	@Test
-	public void testMoverseDejaLibreCeldaAnterior(){
+	public void testMoverPersonajeDejaLibreCeldaAnterior(){
 		Tablero tableroDab = new Tablero();
 		Goku goku = new Goku();
 		tableroDab.colocarPersonaje(5, 6, goku);
@@ -37,7 +38,7 @@ public class PruebasTablero{
 	}
 
 	@Test
-	public void testMoverseHorizontalAlcanceMaximo(){
+	public void testMoverPersonajeHorizontalAlcanceMaximo(){
 		Tablero tableroDab = new Tablero();
 		Goku goku = new Goku();
 		tableroDab.colocarPersonaje(5, 6, goku);
@@ -47,7 +48,7 @@ public class PruebasTablero{
 	}
 	
 	@Test
-	public void testMoverseVerticalAlcanceMaximo(){
+	public void testMoverPersonajeVerticalAlcanceMaximo(){
 		Tablero tableroDab = new Tablero();
 		Goku goku = new Goku();
 		tableroDab.colocarPersonaje(5, 6, goku);
@@ -56,12 +57,54 @@ public class PruebasTablero{
 		assertEquals(tableroDab.columnaPersonaje(goku), 6 + goku.getVelocidad());
 	}
 	@Test
-	public void testMoverseDiagonalAlcanceMaximo(){
+	public void testMoverPersonajeDiagonalAlcanceMaximo(){
 		Tablero tableroDab = new Tablero();
 		Goku goku = new Goku();
 		tableroDab.colocarPersonaje(5, 6, goku);
-		tableroDab.moverPersonaje(5 + ((int)(goku.getVelocidad()/2)), 6 + ((int)(goku.getVelocidad()/2)), goku);
-		assertEquals(tableroDab.filaPersonaje(goku), 5 + ((int)(goku.getVelocidad()/2)));
-		assertEquals(tableroDab.columnaPersonaje(goku), 6 + ((int)(goku.getVelocidad()/2)));
+		tableroDab.moverPersonaje(5 + ((int)(goku.getVelocidad())), 6 + ((int)(goku.getVelocidad())), goku);
+		assertEquals(tableroDab.filaPersonaje(goku), 5 + ((int)(goku.getVelocidad())));
+		assertEquals(tableroDab.columnaPersonaje(goku), 6 + ((int)(goku.getVelocidad())));
 	}
+	@Test(expected = MovimientoInvalido.class)
+	public void testMoverseHorizontalMayorQueAlcanceLanzaMovimientoInvalido(){
+		Tablero tableroDab = new Tablero();
+		Goku goku = new Goku();
+		tableroDab.colocarPersonaje(5, 6, goku);
+		tableroDab.moverPersonaje(5 + goku.getVelocidad() + 1, 6 , goku);
+	}
+	
+	@Test(expected = MovimientoInvalido.class)
+	public void testMoverPersonajeVerticalMayorQueAlcanceLanzaMovimientoInvalido(){
+		Tablero tableroDab = new Tablero();
+		Goku goku = new Goku();
+		tableroDab.colocarPersonaje(5, 6, goku);
+		tableroDab.moverPersonaje(5, 6 + goku.getVelocidad() + 1, goku);
+	}
+	@Test(expected = MovimientoInvalido.class)
+	public void testMoverPersonajeDiagonalMayorAlcanceMaximo(){
+		Tablero tableroDab = new Tablero();
+		Goku goku = new Goku();
+		tableroDab.colocarPersonaje(5, 6, goku);
+		tableroDab.moverPersonaje(5 + 1 + ((int)(goku.getVelocidad())), 6 + ((int)(goku.getVelocidad())), goku);
+	}
+	
+	@Test(expected = MovimientoInvalido.class)
+	public void testMoverPersonajeSobrepasandoOtroLanzaError(){
+		Tablero tableroDab = new Tablero();
+		Goku goku = new Goku();
+		Gohan gohan = new Gohan();
+		tableroDab.colocarPersonaje(5, 6, goku);
+		tableroDab.colocarPersonaje(6, 6, gohan);
+		tableroDab.moverPersonaje(7,6,goku);
+	}
+	@Test(expected = MovimientoInvalido.class)
+	public void testMoverPersonajeSobrepasandoOtroDiagonalLanzaError(){
+		Tablero tableroDab = new Tablero();
+		Goku goku = new Goku();
+		Gohan gohan = new Gohan();
+		tableroDab.colocarPersonaje(5, 7, goku);
+		tableroDab.colocarPersonaje(6, 6, gohan);
+		tableroDab.moverPersonaje(7,5,goku);
+	}
+	
 }
