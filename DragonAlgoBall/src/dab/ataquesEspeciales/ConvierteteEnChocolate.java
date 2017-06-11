578e07b5115a4d86f1a2d41e514603e5c602b44a
Tomás Pinto <tomasp834@gmail.com>
2017-06-11 16:55:29 -0300

    Agrego la especial de Boo

--- DragonAlgoBall/src/dab/ataquesEspeciales/ConvierteteEnChocolate.java
+++ DragonAlgoBall/src/dab/ataquesEspeciales/ConvierteteEnChocolate.java
@@ -1,11 +1,17 @@
 package dab.ataquesEspeciales;
 
+import dab.consumibles.Inutilizador;
 import dab.personajes.Personaje;
 
 public class ConvierteteEnChocolate extends AtaqueEspecial {
 
+	public ConvierteteEnChocolate(Personaje lanzador) {
+		super(lanzador);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
-	public void lanzar(int poderPelea, Personaje enemigo){
-		enemigo.convertirEnChocolate();
+	public void lanzar(Personaje enemigo){
+		enemigo.agregarConsumible(new Inutilizador());
 	}
 }
