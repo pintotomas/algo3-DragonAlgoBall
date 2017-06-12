046a5e9ff8836d9006b3b8e117e4fa76b08668f1
Tomás Pinto <tomasp834@gmail.com>
2017-06-12 14:06:56 -0300

    Fix, nombre de estado incorrecto.

--- DragonAlgoBall/src/dab/estados/gohan/GohanSuperSayajinFase1.java
+++ DragonAlgoBall/src/dab/estados/gohan/GohanSuperSayajinFase1.java
@@ -9,7 +9,7 @@ public class GohanSuperSayajinFase1 extends Estado{
 			poder = 30;
 			alcance = 2;
 			velocidad = 2;
-			nombre = "Gohan Super Sayajin Fase 2";
+			nombre = "Gohan Super Sayajin Fase 1";
 			kiParaTransformar = 30;
 	}
 }
