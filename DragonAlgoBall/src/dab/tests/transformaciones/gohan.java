80d31b98ea98ab32c6a89ae4f5ffc7fe2a44a3f8
Tomás Pinto <tomasp834@gmail.com>
2017-06-11 22:06:26 -0300

    pruebas gohan

--- DragonAlgoBall/src/dab/tests/transformaciones/gohan.java
+++ DragonAlgoBall/src/dab/tests/transformaciones/gohan.java
@@ -22,23 +22,42 @@ public class gohan {
 		goku = new Goku();
 		piccolo = new Piccolo();
 		guerrerosZ = new Equipo("Guerreros Z");
-	
-	}
-	
-	@Before
-	public void setUp() throws Exception {
-		
 		guerrerosZ.agregarPersonaje(gohan);
 		guerrerosZ.agregarPersonaje(goku);
 		guerrerosZ.agregarPersonaje(piccolo);
 		
 	}
-
+	
 	@Test
-	public void primerTransformacionExitosa() {
+	public void primerTransformacionNoDisponible() {
+		assertFalse(gohan.transformarDisponible());
+	}
+	
+	@Test
+	public void primerTransformacionDisponible() {
 		gohan.agregarKi(10);
 		assertTrue(gohan.transformarDisponible());
 	}
-	
-	
+	@Test
+	public void segundaTransformacionNoDisponibleCompanerosConTodaLaVida() {
+		gohan.agregarKi(10);
+		gohan.transformar();
+		assertFalse(gohan.transformarDisponible());
+	}
+	@Test
+	public void segundaTransformacionNoDisponibleCompanerosDanados(){
+		gohan.agregarKi(10);
+		gohan.transformar();
+		goku.agregarHp(-goku.getVida()*0.7);
+		piccolo.agregarHp(-piccolo.getVida()*0.7);
+		assertFalse(gohan.transformarDisponible());
+	}
+	@Test
+	public void segundaTransformacionDisponible(){
+		gohan.agregarKi(10);
+		gohan.transformar();
+		goku.agregarHp(-goku.getVida()*0.8);
+		piccolo.agregarHp(-piccolo.getVida()*0.8);
+		assertTrue(gohan.transformarDisponible());
+	}
 }
