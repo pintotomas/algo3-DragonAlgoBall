3aeaf9a85e18cb1f075ad246ec325f998db6022c
Tomás Pinto <tomasp834@gmail.com>
2017-06-21 15:22:20 -0300

    Agrego musica al inciar el juego

--- DragonAlgoBall/src/vista/ContenedorInicial.java
+++ DragonAlgoBall/src/vista/ContenedorInicial.java
@@ -1,5 +1,10 @@
 package vista;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -10,6 +15,8 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import vista.eventos.BotonJugarEventHandler;
 
@@ -22,7 +29,13 @@ public class ContenedorInicial extends StackPane {
         this.stage = stage;
         this.pixelesAltoHD = pixelesAltoHD;
         this.pixelesAnchoHD = pixelesAnchoHD;
-        
+        try {
+			this.startOpening();
+		} catch (MalformedURLException | URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      
 //        String imagen = "vista/Imagenes/inicioDragonAlgoBall.png";
         ImageView imagen = new ImageView(new Image("vista/Imagenes/inicioDragonAlgoBall.png", stage.getWidth(), stage.getHeight(), true, false));
 //        BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
@@ -41,4 +54,13 @@ public class ContenedorInicial extends StackPane {
 		
         this.getChildren().addAll(imagen, botonJugar);
     }
+	private void startOpening() throws MalformedURLException, URISyntaxException {
+		// TODO Auto-generated method stub
+		  URL resource = ContenedorInicial.class.getResource("/vista/sonidos/opening/DBZOpening.mp3");
+//		  String path_opening = url.getPath();
+//	      Media media = new Media(new File(path_opening).toURI().toString());
+		  Media media = new Media(resource.toString());
+	      MediaPlayer mediaPlayer = new MediaPlayer(media);
+	      mediaPlayer.play();
+	}
 }
